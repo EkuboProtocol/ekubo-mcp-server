70a63bdd9d3881cb60639e85a4d261a653616768
Moody Salem <moody.salem@gmail.com>
2026-08-03 17:11:15 -0400

    chore: release v0.21.5

--- script/smoke.mjs
+++ script/smoke.mjs
@@ -1,6 +1,6 @@
 const origin = (process.argv[2] ?? process.env.MCP_ORIGIN)?.replace(/\/+$/, "");
-const expectedServerVersion = "0.21.4";
-const expectedCatalogRevision = "2026-08-03.revert-decode-plans";
+const expectedServerVersion = "0.21.5";
+const expectedCatalogRevision = "2026-08-03.all-provider-quotes";
 const smokeNonce = `${Date.now()}-${Math.random()}`;
 const privateRecommendationSourcePattern = /dune|8187907|api\.dune/i;
 
